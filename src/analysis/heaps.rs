e244d34685a17f96b5b6b53a3216fdbb7b23038f
Jerry Tworek <jerry@millionintegrals.com>
2024-09-02 21:11:35 -0700

    better formatting.

--- src/analysis/heaps.rs
+++ src/analysis/heaps.rs
@@ -1,11 +1,12 @@
+use csv::Writer;
+
 use crate::ds::heap::binary_heap_vec::BinaryHeapVec;
 use crate::ds::heap::binomial_heap::BinomialHeap;
 use crate::ds::heap::leftist_heap::LeftistHeap;
-use crate::ds::heap::{self as heap, generate_random_vector, Heap};
+use crate::ds::heap::{self as heap, Heap};
 
-use crate::analysis::utils::measure_execution;
+use crate::analysis::utils::{generate_random_vector, measure_execution};
 
-use csv::Writer;
 
 use super::commands::{CommandDescriptor, CommandMap};
 
@@ -27,6 +28,7 @@ fn leftist_heap_measurements() {
     wtr.flush().unwrap();
     println!("Written {filename}")
 }
+
 
 fn leftist_heap_measurements_random() {
     let filename = "data/leftist_heap_insert_random.csv";
@@ -50,47 +52,8 @@ fn leftist_heap_measurements_random() {
 }
 
 
-#[allow(dead_code)]
-fn binary_heap_vec_measurements() {
-    let filename = "data/binary_heap_vec_insert.csv";
-    let mut wtr = Writer::from_path(filename).unwrap();
-    let mut heap = BinaryHeapVec::<i32>::new();
-
-    for n in [10_000, 50_000,  100_000, 150_000, 200_000, 250_000, 300_000, 350_000, 400_000, 450_000, 500_000] {
-        let d = measure_execution(|| {
-            heap::insert_n_elements(&mut heap, n);
-        });
-        heap.clear();
-        wtr.write_record(&[format!("{}", n), format!("{}", d.as_secs_f64())]).unwrap();
-    }
-
-    wtr.flush().unwrap();
-    println!("Written {filename}")
-}
-
-#[allow(dead_code)]
-fn binary_heap_vec_measurements_random() {
-    let filename = "data/binary_heap_vec_insert_random.csv";
-    let mut wtr = Writer::from_path(filename).unwrap();
-    let mut heap = BinaryHeapVec::<i32>::new();
-
-    for n in [10_000, 50_000,  100_000, 150_000, 200_000, 250_000, 300_000, 350_000, 400_000, 450_000, 500_000] {
-        let vec = generate_random_vector(n);
-
-        let d = measure_execution(|| {
-            heap::insert_n_vector_elements(&mut heap, &vec);
-        });
-
-        heap.clear();
-        wtr.write_record(&[format!("{}", n), format!("{}", d.as_secs_f64())]).unwrap();
-    }
-
-    wtr.flush().unwrap();
-    println!("Written {filename}")
-}
 
 
-#[allow(dead_code)]
 fn binomial_heap_measurements() {
     let filename = "data/binomial_heap_insert.csv";
     let mut wtr = Writer::from_path(filename).unwrap();
@@ -108,7 +71,7 @@ fn binomial_heap_measurements() {
     println!("Written {filename}")
 }
 
-#[allow(dead_code)]
+
 fn binomial_heap_measurements_random() {
     let filename = "data/binomial_heap_insert_random.csv";
     let mut wtr = Writer::from_path(filename).unwrap();
@@ -129,12 +92,69 @@ fn binomial_heap_measurements_random() {
     println!("Written {filename}")
 }
 
-fn analyze_leftist_heap() {
+// BINARY HEAP
+
+fn binary_heap_vec_measurements() {
+    let filename = "data/binary_heap_vec_insert.csv";
+    let mut wtr = Writer::from_path(filename).unwrap();
+    let mut heap = BinaryHeapVec::<i32>::new();
+
+    for n in [10_000, 50_000,  100_000, 150_000, 200_000, 250_000, 300_000, 350_000, 400_000, 450_000, 500_000, 700_000, 1_000_000, 2_000_000, 4_000_000] {
+        let d = measure_execution(|| {
+            heap::insert_n_elements(&mut heap, n);
+        });
+        heap.clear();
+        wtr.write_record(&[format!("{}", n), format!("{}", d.as_secs_f64())]).unwrap();
+    }
+
+    wtr.flush().unwrap();
+    println!("Written {filename}")
+}
+
+
+fn binary_heap_vec_measurements_random() {
+    let filename = "data/binary_heap_vec_insert_random.csv";
+    let mut wtr = Writer::from_path(filename).unwrap();
+    let mut heap = BinaryHeapVec::<i32>::new();
+
+    for n in [10_000, 50_000,  100_000, 150_000, 200_000, 250_000, 300_000, 350_000, 400_000, 450_000, 500_000, 700_000, 1_000_000, 2_000_000, 4_000_000] {
+        let vec = generate_random_vector(n);
+
+        let d = measure_execution(|| {
+            heap::insert_n_vector_elements(&mut heap, &vec);
+        });
+
+        heap.clear();
+        wtr.write_record(&[format!("{}", n), format!("{}", d.as_secs_f64())]).unwrap();
+    }
+
+    wtr.flush().unwrap();
+    println!("Written {filename}")
+}
+
+
+// ENTRY POINTS
+
+fn analyze_heap_leftist() {
     leftist_heap_measurements();
     leftist_heap_measurements_random();
 }
 
 
+fn analyze_heap_binomial() {
+    binomial_heap_measurements();
+    binomial_heap_measurements_random()
+}
+
+
+fn analyze_heap_binary() {
+    binary_heap_vec_measurements();
+    binary_heap_vec_measurements_random();
+}
+
+
 pub fn register_commands(cm: &mut CommandMap) {
-    cm.add(CommandDescriptor::new("analyze-heap-leftist", "Run runtime analysis for leftist heap", analyze_leftist_heap))
+    cm.add(CommandDescriptor::new("analyze-heap-leftist", "Run runtime analysis for leftist heap", analyze_heap_leftist));
+    cm.add(CommandDescriptor::new("analyze-heap-binomial", "Run runtime analysis for binomial heap", analyze_heap_binomial));
+    cm.add(CommandDescriptor::new("analyze-heap-binary", "Run runtime analysis for binary heap", analyze_heap_binary));
 }
